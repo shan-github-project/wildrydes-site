1b4ae97560875e88226e149c85bf5ec356b1041e
shan-github-project <svg30.zeeshan@gmail.com>
2023-04-08 21:08:55 +0530

    js file configure user pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_VaWM3zWrI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '48cfa56s5u76ujb3g35vtgc7hd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
